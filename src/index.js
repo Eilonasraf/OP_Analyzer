af4d56b39bfd6e451510ef031e5e0a5c91bdab72
Eilonasraf <eilonasraf@gmail.com>
2025-01-30 13:19:04 +0200

    Updated Code

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const analyzeController = require("./controllers/analyzeController");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // Middleware to parse JSON
+// Middleware to parse JSON 
+app.use(express.json()); 
 
 // Route for fetching raw data 
-app.get("/fetch", fetchDataController.fetchData); // Fetch data route
+app.get("/fetch", fetchDataController.fetchData); 
 
 // Route for analyzing data
-app.get("/analyze", analyzeController.analyzeData); // Analysis route
+app.get("/analyze", analyzeController.analyzeData); 
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
